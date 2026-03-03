87bcdfe7f913bf9e0522e2a7de66796c8589d454
caps2230 <cristhianp.soft@gmail.com>
2026-03-02 19:18:15 -0500

    se creo la conexion el api, y se descifrar el json a una lista dinamica

---
+++ lib/services/api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/personajes.dart';
+
+class Api {
+  static const String _baseUrl = 'https://thesimpsonsapi.com/api/characters';
+
+  Future<List<Personajes>> getPersonajes() async {
+    final response = await http.get(Uri.parse(_baseUrl));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedData = json.decode(response.body);
+      List<dynamic> body = decodedData['results'];
+      return body.map((dynamic item) => Personajes.fromJson(item)).toList();
+    } else {
+      throw ("No se pudo conectar con la API");
+    }
+  }
+}
