e517689dfa4c8edd8cddc2347fb533276d456f35
galeano <jhongaleanomartin@gmail.com>
2026-03-04 16:54:14 -0500

    feat: se agregaron las nuevas rutas de episodios en el llamado a la api

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -3,11 +3,23 @@ import 'package:http/http.dart' as http;
 import '../models/personajes.dart';
 
 class Api {
-  static const String _baseUrl = 'https://thesimpsonsapi.com/api/characters';
-  
+  static const String _urlPersonajes = 'https://thesimpsonsapi.com/api/characters';
+  static const String _urlEpisodios = 'https://thesimpsonsapi.com/api/episodes';
+
 
   Future<List<Personajes>> getPersonajes() async {
-    final response = await http.get(Uri.parse(_baseUrl));
+    final response = await http.get(Uri.parse(_urlPersonajes));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedData = json.decode(response.body);
+      List<dynamic> body = decodedData['results'];
+      return body.map((dynamic item) => Personajes.fromJson(item)).toList();
+    } else {
+      throw ("No se pudo conectar con la API");
+    }
+  }
+  Future<List<Personajes>> getEpisodios() async {
+    final response = await http.get(Uri.parse(_urlEpisodios));
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> decodedData = json.decode(response.body);
