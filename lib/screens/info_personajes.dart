c34f1867770ff7bca2b99dc6bae610be345d0f15
caps2230 <cristhianp.soft@gmail.com>
2026-03-06 06:21:01 -0500

    se cambio el diseño que alguasn secciones de la vista principal

--- lib/screens/info_personajes.dart
+++ lib/screens/info_personajes.dart
@@ -1,5 +1,6 @@
 import '../models/personajes.dart';
 import 'package:flutter/material.dart';
+
 class Detalles extends StatelessWidget {
   final Personajes personajes;
   const Detalles({super.key, required this.personajes});
@@ -7,17 +8,20 @@ class Detalles extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(personajes.nombre)
-      ),
+      appBar: AppBar(title: Text(personajes.nombre)),
       body: SingleChildScrollView(
         child: Center(
-            child: Card(
+          child: Card(
             // ignore: sort_child_properties_last
             child: Container(
               height: 500,
               width: 1200,
-              padding: EdgeInsets.only(top: 15,bottom: 10,left: 19,right: 10),
+              padding: EdgeInsets.only(
+                top: 15,
+                bottom: 10,
+                left: 19,
+                right: 10,
+              ),
 
               child: Row(
                 children: [
@@ -26,30 +30,41 @@ class Detalles extends StatelessWidget {
                     children: [
                       Card(
                         margin: EdgeInsets.zero,
-                        color: Colors.blueGrey, 
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(10),bottomLeft: Radius.circular(10))),
-                        child: SizedBox( 
+                        color: Colors.blueGrey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                          ),
+                        ),
+                        child: SizedBox(
                           height: 470,
                           width: 580,
 
-                        child: Center(
-                          child: Container( 
-                            width: 480,
-                            height: 440,
-                            color: Colors.cyan,
-                            padding: EdgeInsets.only(top: 5,left: 5,right: 5),
-                            child: ClipRRect( 
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                personajes.imagen,
-                                fit: BoxFit.contain,
+                          child: Center(
+                            child: Container(
+                              width: 480,
+                              height: 440,
+                              color: Colors.cyan,
+                              padding: EdgeInsets.only(
+                                top: 5,
+                                left: 5,
+                                right: 5,
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+
+                                child: Hero(
+                                  tag: personajes.nombre,
+                                  child: Image.network(
+                                    personajes.imagen,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-
                         ),
-                        ),
-                        
                       ),
                     ],
                   ),
@@ -58,22 +73,24 @@ class Detalles extends StatelessWidget {
                     children: [
                       Card(
                         margin: EdgeInsets.zero,
-                        color: Colors.white, 
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topRight: Radius.circular(10),bottomRight: Radius.circular(10))),
-                        child: SizedBox( 
-                          height: 470,
-                          width: 580,
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          ),
                         ),
+                        child: SizedBox(height: 470, width: 580),
                       ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
             color: Colors.amberAccent,
           ),
         ),
-      )
+      ),
     );
   }
 }
